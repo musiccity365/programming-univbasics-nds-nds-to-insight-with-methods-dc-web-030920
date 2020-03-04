d1fcdd9c722dfb3c5aef070abf300ab05aaccce4
Michael David Sackstein <m_sackstein@yahoo.com>
2020-03-04 20:11:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,6 +6,7 @@ require 'pry'
 # The return value should be like:
 #
 # { directorOne => allTheMoneyTheyMade, ... }
+
 def directors_totals(nds)
   result = {}
   nil
@@ -26,6 +27,7 @@ end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
+
 def gross_for_director(director_data)	
    movie_index = 0 
     gross_total = 0 
