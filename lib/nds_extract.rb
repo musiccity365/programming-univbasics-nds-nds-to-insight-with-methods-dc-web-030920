5e0aaf8f2ba04f767e38b2b36c5cae5e9ac643e3
Michael David Sackstein <m_sackstein@yahoo.com>
2020-03-04 20:08:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -14,7 +14,19 @@ end
 def directors_totals(nds)
   result = {}
   nil
-  
+  director_index = 0 
+
+  while director_index < nds.count do
+    movie_index = 0 
+    gross_total = 0 
+    while movie_index < nds[director_index][:movies].length do
+      gross_total += nds[director_index][:movies][movie_index][:worldwide_gross]
+      movie_index += 1 
+    end
+    result[nds[director_index][:name]] = gross_total
+    director_index += 1 
+  end
+  result
 end
 def gross_for_director(director_data)	
 end
